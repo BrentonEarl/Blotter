10cd415fe711a2a6b24d5f6c62bfcdd39420ba7a
Brent Earl <brent@exitstatusone.com>
2014-06-05 15:11:20 -0500

    Tested Haml, decided not to use it

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,19 @@ print "\n\n"
 print "Seed:  Created 1 Test Post."
 print "\n"
 if Posts.count == 0
-  5.times do
-      post = Posts.new
-      category = Category.new
-    
-      post.title = "Testing"
-      post.author = "Brenton Earl"
-      post.summary = "Welcome to the site.  This is a test post."
-      post.body = "Welcome to the site.  This is a test post."
-      # Add category
-      category.name = "Test"
-      post.categories << category
-      
-      post.save
-  end
-  print " Created posts."
+  post = Posts.new
+  category = Category.new
+  
+  post.title = "First Update"
+  post.author = "Brenton Earl"
+  post.summary = "Welcome to the new site."
+  post.body = "Welcome to the site.  Over the next few days there may be a some changes in the event that something does not work as expected.  For all intensive purposes though, the site is launched.  Please bookmark the site and keep coming back!"
+  # Add category
+  category.name = "Update"
+  post.categories << category
+
+  post.save
+  print " Created post."
 else
   print " Post not created.  It already exists"
 end
@@ -31,13 +29,11 @@ print "\n"
 if SiteSettings.count == 0
   SiteSettings.create(
   name: "Exit Status One",
-  tagline: "This is the tagline.",
+  tagline: "News, Linux, Ruby, Music",
   author: "Brenton Earl",
-  meta_description: "The one stop shop for all things Linux and Ruby.",
-  meta_keywords: "Linux, Ruby, Sinatra, Rails, SQL, HTML, CSS",
-  about_site: "This site was developed by Brenton Earl.  
-              It uses the Sinatra DSL, which is written in the 
-              Ruby programming language."
+  meta_description: "A compilation on information about music, Linux, Ruby programming and interesting news.",
+  meta_keywords: "Linux, Ruby, Sinatra, Rails, SQL, HTML, CSS, Music, Punk, Rock, Indie",
+  about_site: "This site was developed by Brenton Earl.  It uses the Sinatra DSL, which is written in the Ruby programming language."
   )
   print " Created Default Site Settings"
 end
